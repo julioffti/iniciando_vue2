b0cd8b4e137037247a780dea25b038a58663059e
julio <julio_filgueiras@live.com>
2017-12-20 22:41:28 -0200

    segundo commit

--- src/main.js
+++ src/main.js
@@ -60,6 +60,10 @@ let meuVue = new Vue({
       let indexCasa = Math.floor(Math.random() * 20),
         indexFora = Math.floor(Math.random() * 20);
 
+        while( (indexFora == indexCasa)){
+            indexFora = Math.floor(Math.random() * 20);
+        }
+
       this.novoJogo.casa.time = this.times[indexCasa];
       this.novoJogo.casa.gols = 0;
       this.novoJogo.fora.time = this.times[indexFora];
